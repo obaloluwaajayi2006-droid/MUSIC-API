9414207fba4b236a025aab7b8135674d2e7dd586
emmanuel123 <obaloluwaajayi2006@gmail.com>
2026-03-30 16:06:22 -0700

    third commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors({
-  origin: 'https://localhost:5173'
+  origin: 'http://localhost:5173'
 }));
 
 const port = 2000;
